555e0df6e7088961e8f23d3dd1e1b9ce5c26ddf6
Timothy W. Frakes <timf2004@gmail.com>
2017-10-22 10:19:46 -0500

    Added Console App Project for easy interaction with the Harlow Library.

---
+++ Harlow/HarlowConsoleTestbed/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarlowConsoleTestbed {
+	class Program {
+		static void Main(string[] args)
+		{
+			try{
+				Console.WriteLine("Starting Harlow interactions.");
+			} finally{
+				Console.WriteLine("Press any key to exit.");
+				Console.ReadKey();
+			}
+		}
+	}
+}
